84369d14b7b921008b45dae30da97a332ad92df0
Ghufran <tabishmahmood445@gmail.com>
2025-07-03 18:56:48 +0530

    added search functionality using searchContext and adding filterNews in Fav/news section , now user can serach in favs

--- src/pages/fav/Newsfav.tsx
+++ src/pages/fav/Newsfav.tsx
@@ -2,18 +2,43 @@ import { useSelector } from "react-redux";
 import type { RootState } from "../../store/store";
 import NewsCard from "../../components/FeedCards/NewsCard";
 import type { NewsArticle } from "../../types";
+import { useContext, useEffect, useState } from "react";
+import SearchContext from "../../contexts/SearchContext";
+
+const filterNews = (
+    allNews: NewsArticle[],
+    setFavNews: React.Dispatch<React.SetStateAction<NewsArticle[]>>,
+    searchQuery: string
+) => {
+    if (searchQuery.trim() === "") {
+        setFavNews(allNews);
+    } else {
+        const filteredNews = allNews.filter(
+            (news: NewsArticle) =>
+                news.title &&
+                news.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        setFavNews(filteredNews);
+    }
+};
 
 const NewsFav = () => {
     const selectedFavNews = useSelector(
         (store: RootState) => store.fav.favNews as []
     );
 
+    const [favNews, setFavNews] = useState<NewsArticle[]>(selectedFavNews);
+    const { searchQuery } = useContext(SearchContext);
+
+    useEffect(() => {
+        filterNews(selectedFavNews, setFavNews, searchQuery);
+    }, [searchQuery]);
     return (
         <div>
             <h1 className="text-2xl font-bold mb-4">Favorite News</h1>
-            {selectedFavNews.length > 0 ? (
+            {favNews.length > 0 ? (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {selectedFavNews.map((news: NewsArticle) => (
+                    {favNews.map((news: NewsArticle) => (
                         <NewsCard key={news.id} article={news} />
                     ))}
                 </div>
